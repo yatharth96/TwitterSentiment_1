9963284a65ea6cb318b40993f1f21b1d17215f80
Yatharth Malik <yatharthmalik.96@gmail.com>
2016-07-28 21:45:11 +0530

    Update main.py

--- files/main.py
+++ files/main.py
@@ -1,7 +1,12 @@
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
-import time
+
+#Twitter credentials.Get them from https://apps.twitter.com
+CONSUMER_KEY=""
+CONSUMER_SECRET=""
+ACESS_TOKEN=""
+ACCESS_TOKEN_SECRET=""
 
 class listener(StreamListener):    
     def on_data(self,data):
@@ -14,7 +19,7 @@ class listener(StreamListener):
         fh.write(tweet)
         fh.write('\n')
         fh.write("Sentimental Score: "+str(sent)+"\n")
-        fh.write("======================================================================================")
+        fh.write("=======================================================================================================")
         fh.write("\n")         
         fh.close()
         return True
@@ -22,12 +27,13 @@ class listener(StreamListener):
     def on_error(self,status):
         print status
 
-auth=OAuthHandler("TiSNgNuLqwJ6zizM6uuC1ggOg","qrwe5VNUevtrU0t9lvlv0z6w9J97PNosHQ9O8Orn9yFf0EdsYZ")
-auth.set_access_token("443542132-SM0FVcmbabPyTFmf8Fi0BfCCfV5Nbyblm39hT2sB","3fEcym0sP3UtnhkhIVvgEGf5zbC646S9B1dDSaFL9doi2")
+auth=OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
 
-wordDB=dict(map(lambda (k,v):(k,int(v)),[line.split('\t') for line in open("AFINN-111.txt")]))
+wordDB=dict(map(lambda (k,v):(k,int(v)),[line.split('\t') for line in open("vocabDB.txt")]))
 
 twitterStream=Stream(auth,listener())
-twitterStream.filter(track=["Sultan"],languages=["en"])
+### Enter the tags you want to search the tweets about. 
+twitterStream.filter(track=["<Enter tags here>"],languages=["en"])
 
     
